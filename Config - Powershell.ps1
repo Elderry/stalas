97e814dfd295fbfdf0c0eb30ddedcf7592e52f98
Ruiyang <Elderry@outlook.com>
2018-01-28 19:00:55 +0800

    Copy profile to both powershell native and powershell core.

--- Config - Powershell.ps1
+++ Config - Powershell.ps1
@@ -1,2 +1,4 @@
-$target = Join-Path $Env:UserProfile 'Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1'
-Copy-Item 'Profile - Powershell.ps1' $target
+$nativeProfile = Join-Path $Env:UserProfile 'Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1'
+$coreProfile = Join-Path $Env:UserProfile 'Documents\PowerShell\Microsoft.PowerShell_profile.ps1'
+Copy-Item 'Profile - Powershell.ps1' $nativeProfile
+Copy-Item 'Profile - Powershell.ps1' $coreProfile
