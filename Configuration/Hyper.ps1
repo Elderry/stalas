84efe86feef354aeb37143f28092536c7552bd5f
Ruiyang Li <Elderry@outlook.com>
2018-05-03 17:09:52 +0800

    Improved Hyper config experience.

--- Configuration/Hyper.ps1
+++ Configuration/Hyper.ps1
@@ -1,3 +1,9 @@
 $shell = "shell: '" + ((Join-Path $Env:PWSH_HOME 'pwsh.exe') -replace '\\', '\\') + "'"
 $settings = Join-Path $Env:UserProfile '.hyper.js'
-(Get-Content 'Settings\Hyper.js') -replace "shell:\s*'.*'", $shell | Set-Content $settings
+$content = (Get-Content 'Settings\Hyper.js') -replace "shell:\s*'.*'", $shell
+
+if ((Test-Path $settings) -And ([String]::Concat((Get-Content $settings)) -eq [String]::Concat($content))) {
+    exit
+}
+
+Set-Content -Value $content -Path $settings
