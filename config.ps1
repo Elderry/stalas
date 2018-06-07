a4b31fc51e797dd8ec0a5a89c235802591e8f431
Ruiyang Li <Elderry@outlook.com>
2018-06-07 23:00:51 +0800

    Now config.ps1 is able to pass arguments into individual scripts.

--- config.ps1
+++ config.ps1
@@ -1,47 +1,52 @@
-#Requires -RunAsAdministrator
-
 param (
     [ValidateSet("Windows Console", "PowerShell", "Visual Studio Code", "Bash", "Vim", "Hyper", "Registry")]
-    [string[]] $targets
+    [string] $target
 )
 
-function Write-Split([string] $prefix, [string] $key, [string] $suffix) {
-    $total = 50
-    $length = $prefix.Length + $key.Length + $suffix.Length
-    $hyphen = ($total - $length) / 2
-    $hyphen_before = '-' * [Math]::Floor($hyphen)
-    $hyphen_after  = '-' * [Math]::Ceiling($hyphen)
-    Write-Host $hyphen_before -ForegroundColor 'DarkBlue'  -NoNewLine
-    Write-Host " $prefix["    -ForegroundColor 'DarkGreen' -NoNewLine
-    Write-Host "$key"         -ForegroundColor 'DarkRed'   -NoNewLine
-    Write-Host "]$suffix "    -ForegroundColor 'DarkGreen' -NoNewLine
-    Write-Host $hyphen_after  -ForegroundColor 'DarkBlue'
-}
-
-$pwsh = 'C:\Program Files\PowerShell'
-if (Test-Path $pwsh) {
-    Get-ChildItem $pwsh | ForEach-Object {
-        if ($_.Name -match '\d+(\.\d+)*') {
-            [Environment]::SetEnvironmentVariable('PWSH_HOME', (Join-Path $pwsh $_.Name), 'User')
+$pwshInstalled = $false
+if ($IsWindows) {
+    $pwsh = 'C:\Program Files\PowerShell'
+    if (Test-Path $pwsh) {
+        Get-ChildItem $pwsh | ForEach-Object {
+            if ($_.Name -match '\d+(\.\d+)*') {
+                $pwshInstalled = $true
+                [Environment]::SetEnvironmentVariable('PWSH_HOME', (Join-Path $pwsh $_.Name), 'User')
+            }
         }
     }
-} else {
-    Write-Host -ForegroundColor 'Red' 'Failed to configure, please install Powershell Core first.'
+}
+
+if (-not $pwshInstalled) {
+    Write-Host -ForegroundColor 'Red' 'Failed to configure, please install PowerShell Core first.'
     Write-Host 'Link: https://github.com/PowerShell/PowerShell/releases'
     exit
 }
 
-Write-Host "`n--------------- Elderry's Config Files ---------------" -ForegroundColor 'DarkBlue'
+$width = 80
 
-function Config([string] $name, [string] $type) {
+function Write-Split([string] $prefix, [string] $key, [string] $suffix) {
+    $length = $prefix.Length + $key.Length + $suffix.Length + 4
+    $hyphen = ($width - $length) / 2
+    Write-Host ('-' * [Math]::Floor($hyphen)) -ForegroundColor 'DarkBlue' -NoNewLine
+    Write-Host " $prefix[" -ForegroundColor 'DarkGreen' -NoNewLine
+    Write-Host "$key" -ForegroundColor 'DarkRed' -NoNewLine
+    Write-Host "]$suffix " -ForegroundColor 'DarkGreen' -NoNewLine
+    Write-Host ('-' * [Math]::Ceiling($hyphen)) -ForegroundColor 'DarkBlue'
+}
+
+function Config([string] $name, [string] $type, [string[]] $arguments) {
     Write-Host
     Write-Split 'Going to config ' $name '.'
     switch ($type) {
-        'ps1' { &  "Configuration\$name.$_" }
-        'sh'  { bash "Configuration/$name.$_" }
+        'ps1' { & "Configuration/$name.$_" @arguments }
+        'sh' { bash "Configuration/$name.$_" @arguments }
     }
     Write-Split 'Configuration of ' $name ' finished.'
 }
+
+$hyphen = ($width - $Env:UserName.Length - 17) / 2
+$banner = "$('-' * [Math]::Floor($hyphen)) $Env:UserName's Config Files $('-' * [Math]::Floor($hyphen))"
+Write-Host "`n$banner" -ForegroundColor 'DarkBlue'
 
 $configs = @{}
 Get-ChildItem 'Configuration' | Select-Object -ExpandProperty 'Name' | ForEach-Object {
@@ -49,12 +54,10 @@ Get-ChildItem 'Configuration' | Select-Object -ExpandProperty 'Name' | ForEach-O
     $configs[$name[0]] = $name[1]
 }
 
-if ($targets.Length -ne 0) { 
-    $complete_configs = $configs.Clone()
-    $complete_configs.GetEnumerator() | ForEach-Object {
-        if (-not $targets.Contains($_.Name)) { $configs.Remove($_.Name) }
-    }
+if ($target -ne '') {
+    Config $target $configs[$target] $args
+} else {
+    $configs.GetEnumerator() | ForEach-Object { Config $_.Name $_.Value }
 }
-$configs.GetEnumerator() | ForEach-Object { Config $_.Name $_.Value }
 
-Write-Host "`n--------------- Elderry's Config Files ---------------`n" -ForegroundColor 'DarkBlue'
+Write-Host "`n$banner`n" -ForegroundColor 'DarkBlue'
