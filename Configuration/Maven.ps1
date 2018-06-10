43506e9d56fddd0dd3121db0f32d8a828af95eba
Ruiyang Li <Elderry@outlook.com>
2018-06-10 11:28:56 +0800

    Added `Maven`'s xml setting files.

--- Configuration/Maven.ps1
+++ Configuration/Maven.ps1
@@ -3,16 +3,23 @@ param(
     [string] $environment = 'TSCN'
 )
 
-$private = [IO.Path]::GetFullPath("$PSScriptRoot/../Resources/Certificates/Private")
+Import-Module powershell-yaml
+
+$private = [IO.Path]::GetFullPath("$PSScriptRoot/../Resources/Private")
+$credentials = Get-Content "$private/Credentials.yml" -Raw | ConvertFrom-Yaml
 switch ($environment) {
     'TSCN' {
         $opts = "-Djavax.net.ssl.keyStore=$private\lry@cn.tradeshift.com.pfx " `
-            + "-Djavax.net.ssl.keyStoreType=pkcs12 -Djavax.net.ssl.keyStorePassword=112233"
+            + "-Djavax.net.ssl.keyStoreType=pkcs12 -Djavax.net.ssl.keyStorePassword=$($credentials["$_-key-pair"])"
     }
     'TS' {
         $opts = "-Djavax.net.ssl.keyStore=$private\lry@tradeshift.com.pfx " `
-            + "-Djavax.net.ssl.keyStoreType=pkcs12 -Djavax.net.ssl.keyStorePassword=231210"
+            + "-Djavax.net.ssl.keyStoreType=pkcs12 -Djavax.net.ssl.keyStorePassword=$($credentials["$_-key-pair"])"
     }
 }
-
 [Environment]::SetEnvironmentVariable('MAVEN_OPTS', $opts, 'User')
+
+(Get-Content "$PSScriptRoot/../Settings/Maven - $environment.xml") `
+    -replace '{user}', $credentials["LDAP-$environment-user"] `
+    -replace '{password}', $credentials["LDAP-$environment-password"] |
+    Set-Content '~/.m2/settings.xml'
