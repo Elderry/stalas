792c0763b5412cce29d79429f1b6b0866834693b
Ruiyang Li <Elderry@outlook.com>
2018-06-25 11:07:19 +0800

    Added vscode keybindings config for macOS.

--- Configurations/Visual Studio Code/Configuration.ps1
+++ Configurations/Visual Studio Code/Configuration.ps1
@@ -6,7 +6,9 @@ if ($IsWindows) {
     $discard = '\s+//\[macOS\]'
 } elseif ($IsMacOS) {
     $shell = "$PSHOME/pwsh"
-    $settings = '~/Library/Application Support/Code/User/settings.json'
+    $user ='~/Library/Application Support/Code/User'
+    $settings = "$user/settings.json"
+    $keybindings = "$user/keybindings.json"
     $os = 'osx'
     $keep = '\s+//\[macOS\]'
     $discard = '\s+//\[Windows\]'
@@ -17,3 +19,7 @@ $content = (Get-Content "$PSScriptRoot/settings.json") `
     -replace "(`"terminal\.integrated\.shell\.$os`")\s*:\s*`".*`"", "`$1: `"$shell`"" `
     -replace $keep -notmatch $discard |
     Set-Content $settings
+
+if ($IsMacOS) {
+    Copy-Item "$PSScriptRoot/keybindings.json" $keybindings
+}
