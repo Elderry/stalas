e67ea250a959c018cee07818fc78e9c659394e46
Ruiyang Li <Elderry@outlook.com>
2018-07-02 17:35:37 +0800

    Added BWTS's cert config for docker.

--- Configurations/Docker/Configuration.ps1
+++ Configurations/Docker/Configuration.ps1
@@ -1,8 +1,15 @@
-$target = if ($IsWindows) { "$Env:ProgramData/Docker/" } `
-    elseif ($IsMacOS) { '~/.docker' }
-$target = "$target/certs.d/docker.tradeshift.net"
+$resources = "$PSScriptRoot/../../Resources/Certificates"
+$private = "$PSScriptRoot/../../Resources/Private"
 
-New-Item -ItemType 'directory' $target -Force | Out-Null
-Copy-Item "$PSScriptRoot/../../Resources/Certificates/Root - TS.crt" "$target/ca.crt"
-Copy-Item "$PSScriptRoot/../../Resources/Certificates/client.cert" "$target/client.cert"
-Copy-Item "$PSScriptRoot/../../Resources/Private/client.key" "$target/client.key"
+$certificates = '~/.docker/certs.d'
+$TSDomain = "$certificates/docker.tradeshift.net"
+$TSCNDomain = "$certificates/registry.bwtsi.cn"
+
+New-Item -ItemType 'directory' $TSDomain -Force | Out-Null
+New-Item -ItemType 'directory' $TSCNDomain -Force | Out-Null
+Copy-Item "$resources/Root - TS.crt" "$TSDomain/ca.crt"
+Copy-Item "$resources/Root - TSCN.crt" "$TSCNDomain/ca.crt"
+Copy-Item "$resources/Certificate Bundle - TS.pem" "$TSDomain/client.cert"
+Copy-Item "$resources/Certificate Bundle - TSCN.pem" "$TSCNDomain/client.cert"
+Copy-Item "$private/Private Key - TS.pem" "$TSDomain/client.key"
+Copy-Item "$private/Private Key - TSCN.pem" "$TSCNDomain/client.key"
