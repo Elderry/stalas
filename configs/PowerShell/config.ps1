ead6b3ec3cfd5465e06b2e9ba5c807bb9603a2d8
Ruiyang Li <Elderry@outlook.com>
2019-03-31 19:44:00 +0800

    Fix PowerShell config for first run.

--- configs/PowerShell/config.ps1
+++ configs/PowerShell/config.ps1
@@ -8,13 +8,16 @@ if ($IsMacOS) {
 $profile = "$PSScriptRoot/Microsoft.PowerShell_profile.ps1"
 if ($IsWindows) {
     $content = (Get-Content $profile) -replace '\s+#\[Windows\]' -notmatch '\s+#\[macOS\]'
-    $core = '~/Documents/PowerShell/Microsoft.PowerShell_profile.ps1'
-    $native = '~/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1' 
+    $coreDir = '~/Documents/PowerShell'
+    $nativeDir = '~/Documents/WindowsPowerShell'
+    $core = "$coreDir/Microsoft.PowerShell_profile.ps1"
+    $native = "$nativeDir/Microsoft.PowerShell_profile.ps1" 
+    New-Item -ItemType 'directory' $coreDir -Force | Out-Null
+    New-Item -ItemType 'directory' $nativeDir -Force | Out-Null
     Set-Content ($core, $native) $content
 } elseif ($IsMacOS) {
     $content = (Get-Content $profile) -replace '\s+#\[macOS\]' -notmatch '\s+#\[Windows\]'
     $core = '~/.config/powershell/Microsoft.PowerShell_profile.ps1'
     New-Item -ItemType 'directory' '~/.config/powershell/' -Force | Out-Null
     Set-Content $core $content
-    scutil --set HostName 'Monoceros'
 }
