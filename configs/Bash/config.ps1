0b9520183218b69b928fb5eef283b810ba82747a
Ruiyang Li <Elderry@outlook.com>
2019-12-05 22:56:43 +0800

    Add Validate-OneDrive-Path funciton.

--- configs/Bash/config.ps1
+++ configs/Bash/config.ps1
@@ -1,11 +1,16 @@
+. "$PSScriptRoot/../common.ps1
+
+Validate-OneDrive-Path
+
 if ($IsLinux) {
     $content = Get-Content "$PSScriptRoot/ubuntu-base.bashrc", "$PSScriptRoot/ubuntu-custom.bashrc"
     $target = '.bashrc'
 } elseif ($IsMacOS) {
-    $target = '.bashrc'
+    $target = '.bash_profile'
 }
 
-$mavenKeyStorePassword = Get-Content '~/OneDrive/Collections/AppBackup/Tradeshift/lry@cn.tradeshift.com.pfx.pass.txt'
+$mavenKeyStorePassword = Get-Content `
+    (Join-Path $oneDrive 'Collections/AppBackup/Tradeshift/lry@cn.tradeshift.com.pfx.pass.txt')
 
 $content -replace '<key store password>', $mavenKeyStorePassword |
     Set-Content "~/$target"
--- configs/common.ps1
+++ configs/common.ps1
@@ -2,3 +2,15 @@ function New-Directory([string] $name) {
     if (Test-Path $name) { return }
     New-Item -ItemType 'directory' $name -Force | Out-Null
 }
+
+function Validate-OneDrive-Path() {
+    if (Test-Path '~/OneDrive') {
+        $script:oneDrive = '~/OneDrive'
+    } elseif (Test-Path '/mnt/c/Windows') {
+        $user = $(cmd.exe /c 'echo %USERNAME%')
+        $script:oneDrive = "/mnt/c/Users/$user/OneDrive"
+    } else {
+        Write-Error 'Failed to detect a valid OneDrive path, exiting...'
+        exit
+    }
+}
