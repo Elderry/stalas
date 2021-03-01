bfba42814a9a9c0747dd1751a2728a00600cc53e
Ruiyang Li <Elderry@outlook.com>
2021-03-01 10:42:50 +0800

    Fix profile config for PowerShell.

--- configs/PowerShell - Windows/config.ps1
+++ configs/PowerShell - Windows/config.ps1
@@ -6,6 +6,5 @@ Configure operation for Windows PowerShell.
 $ProfileDir = '~/Documents/PowerShell'
 if (!(Test-Path $ProfileDir)) { New-Item -ItemType 'Directory' $ProfileDir }
 
-$profileName = 'Microsoft.PowerShell_profile.ps1'
-$content = Get-Content "$PSScriptRoot/$profileName"
-Set-Content "$coreDir/$profileName" $content
+$ProfileName = 'Microsoft.PowerShell_profile.ps1'
+Copy-Item "$PSScriptRoot/$ProfileName" "$ProfileDir/$ProfileName"
