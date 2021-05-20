7f840fef18b4a8c61d81d8b5e18ebaf38d31c57e
Ruiyang Li <Elderry@outlook.com>
2021-05-20 16:00:42 +0800

    Fix one path bug for ssh config.

--- configs/SSH/config.ps1
+++ configs/SSH/config.ps1
@@ -3,9 +3,9 @@
 Configure operation for SSH.
 #>
 
-if (!(Test-Path "$Env:HOME/.ssh")) {
+if (!(Test-Path "$Env:UserProfile/.ssh")) {
     Write-Warning 'Directory [~/.ssh] doesn''t exist, creating...'
-    New-Item -Type 'Directory' "$Env:Home/.ssh"
+    New-Item -Type 'Directory' "$Env:UserProfile/.ssh"
 }
 
-Copy-Item "$PSScriptRoot/config" "$Env:HOME/.ssh/config"
+Copy-Item "$PSScriptRoot/config" "$Env:UserProfile/.ssh/config"
